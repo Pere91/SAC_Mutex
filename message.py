c9f0777bb850a1e41406cde87b4249475a1217a8
Pere91 <peregm.91@gmail.com>
2025-11-12 22:01:21 +0100

    Comments on message and node

--- message.py
+++ message.py
@@ -1,8 +1,10 @@
 import json
-
-from enum import Enum # Added
+from enum import Enum
  
 class Message_type(Enum):
+    """
+    All the message types available.
+    """
     FAILED = 0
     YIELD = 1
     INQUIRE = 2
@@ -11,6 +13,16 @@ class Message_type(Enum):
     RELEASE = 5
 
 class Message(object):
+    """
+    Represents a message sent between nodes.
+
+    Attributes:
+        msg_type (Message_Type): Type of the message.
+        src (int): Id of the message sender.
+        dest (int): Id of the message receiver.
+        ts (int): Lamport timestamp of the message sending.
+        data (any, optional): Content of the message. Defaults to None.
+    """
     def __init__(self,
             msg_type=None,
             src=None,
@@ -18,6 +30,16 @@ class Message(object):
             ts=None,
             data=None,
             ):
+        """
+        Constructor for class Message.
+
+        Args:
+            msg_type (Message_Type): Type of the message. Defaults to None.
+            src (int): Id of the message sender. Defaults to None.
+            dest (int): Id of the message receiver. Defaults to None.
+            ts (int): Lamport timestamp of the message sending. Defaults to None.
+            data (any, optional): Content of the message. Defaults to None.
+        """
         self.msg_type = msg_type
         self.src = src
         self.dest = dest
@@ -25,6 +47,12 @@ class Message(object):
         self.data = data
 
     def __json__(self):
+        """
+        Put Message in JSON format.
+
+        Returns:
+            dict: JSON with the fields of the message.
+        """
         return dict(msg_type=self.msg_type, 
             src=self.src,
             dest=self.dest, 
@@ -32,24 +60,66 @@ class Message(object):
             data=self.data)
     
     def __str__(self):
+        """
+        Put Message in string format.
+
+        Returns:
+            str: String displaying the fields of the message.
+        """
         return f"Message({self.msg_type}, {self.src}, {self.dest}, {self.ts}, {self.data})"
 
     def set_type(self, msg_type):
+        """
+        Setter for msg_type field.
+
+        Args:
+            msg_type (Message_type): Type which to update the message to.
+        """
         self.msg_type = msg_type
 
     def set_src(self, src):
+        """
+        Setter for src field.
+
+        Args:
+            src (int): Updated value for the source of the message.
+        """
         self.src = src
 
     def set_dest(self, dest):
+        """
+        Setter for dest field.
+
+        Args:
+            dest (int): Updated value for the destination of the message.
+        """
         self.dest = dest
 
     def set_ts(self, ts):
+        """
+        Setter for ts field.
+
+        Args:
+            ts (int): Updated value for the Lamport timestamp of the message.
+        """
         self.ts = ts
 
     def set_data(self, data):
+        """
+        Setter for the data field.
+
+        Args:
+            data (any): New data content for the message.
+        """
         self.data = data
 
     def to_json(self):
+        """
+        Serializes the JSON representation of the Message.
+
+        Returns:
+            str: Serialized JSON.
+        """
         obj_dict = dict()
         obj_dict['msg_type'] = self.msg_type.value
         obj_dict['src'] = self.src
@@ -60,6 +130,15 @@ class Message(object):
 
     @staticmethod
     def from_json(msg):
+        """
+        Converts a JSON back into a Message.
+
+        Args:
+            msg (dict): Jsonified Message.
+
+        Returns:
+            Message: Message with the fields in the input JSON.
+        """
         return Message(
             msg_type=Message_type(msg['msg_type']),
             src=msg['src'],
@@ -70,20 +149,37 @@ class Message(object):
     
     @staticmethod
     def parse(str):
+        """
+        Parses a serialized jsonified Message to identify possible joined
+        back-to-back JSONs. In case of finding any, splits them in single
+        serialized JSONs.
+
+        Args:
+            str (str): Serialized stream with one or more JSONs.
+
+        Raises:
+            ValueError: If some JSON does not end in }.
+
+        Returns:
+            list: All the serialized JSONs separated.
+        """
         msgs = []
 
         while True:
+            # Find if there is some back-to-back JSONs
             split = str.find("}{")
 
+            # If there aren't and JSON format is correct, end
             if split == -1:
                 if str[-1] != '}':
                     raise ValueError("JSON must end in }")
                 msgs.append(str)
                 break
             
+            # If there are, split the JSON from the rest of the stream and
+            # repeat until there aren't.
             if str[:split + 1][-1] != '}':
                 raise ValueError("JSON must end in }")
-
             msgs.append(str[:split + 1])
             str = str[split + 1:]
 
--- node.py
+++ node.py
@@ -1,79 +1,103 @@
-from threading import Event, Thread, Timer, Condition
-from datetime import datetime, timedelta
+from threading import Thread, Condition
 from math import ceil, sqrt
 import time
 from nodeServer import NodeServer
 from nodeSend import NodeSend
 from message import Message, Message_type
 import config
-import random 
 
 class Node(Thread):
+    """
+    Represents a Node of the distributed system.
+
+    Attributes:
+        id (int): Numerical identifier of the Node.
+        port (int): Node's port.
+        daemon (bool): Thread's daemon option.
+        lamport_ts (int): Lamport timestamp of the last message sent.
+        server (NodeServer): Server for handling the incoming messages.
+        client (Nodesend): Client for handling message sending.
+        collegues (list): Colleagues in the Node's quorum.
+    """
     _FINISHED_NODES = 0
     _HAVE_ALL_FINISHED = Condition()
 
     def __init__(self,id):
+        """
+        Constructor for class Noed.
+
+        Args:
+            id (int): Numerical identifier of the Node.
+        """
         Thread.__init__(self)
         self.id = id
         self.port = config.port+id
         self.daemon = True
         self.lamport_ts = 0
-
+        self.__form_colleagues()
         self.server = NodeServer(self) 
         self.server.start()
-
-        self.__form_colleagues()
         self.client = NodeSend(self) 
 
     def __form_colleagues(self):
-        num_rows = ceil(sqrt(config.numNodes))
+        """
+        Form the quorum for the Node. If all nodes are displayed in a NxN
+        matrix, row-major ordered by node id, colleagues shall be those on the
+        same row or on the same column as the Node. Since Maekawa's algorithm
+        requires all quora to have the same number of nodes, there must be a
+        number of nodes that makes full rows.
+        """
 
+        # The dimension of the matrix rounded op from the square root of the
+        # number of nodes.
+        num_rows = ceil(sqrt(config.numNodes))
         colleague_matrix = []
         cont = True
 
+        # Form all rows
         for i in range(num_rows):
             if not cont:
                 break
 
+            # Form each row
             row = []
             for j in range(num_rows):
                 pos = i * num_rows + j
 
+                # When the position reaches the number of nodes, finish
                 if  pos >= config.numNodes:
                     cont = False
                     break
 
                 row.append(i * num_rows + j)
-
             colleague_matrix.append(row)
 
+        # List colleagues from the same row and the same column as the Node
         row_colleagues = [i for row in colleague_matrix for i in row if self.id in row]
         col_colleagues = [i for row in colleague_matrix for i in row if (i % num_rows) == (self.id % num_rows) and i != self.id]
 
+        # Create quorum
         self.collegues = []
-
         for i in row_colleagues:
             if i != self.id:
                 self.collegues.append(i)
         for i in col_colleagues:
             self.collegues.append(i)
 
-        
-
 
     def do_connections(self):
+        """
+        Connect to all other nodes via socket.
+        """
         self.client.build_connection()
 
     def run(self):
+        """
+        Run simulacrum scenario of multiple accesses to a critical section
+        using Maekawa's algorithm for mutual exclusion.
+        """
         print("Run Node%i with the follows %s"%(self.id,self.collegues))
         self.client.start()
-
-        
-        #TODO MANDATORY Change this loop to simulate the Maekawa algorithm to 
-        # - Request the lock
-        # - Wait for the lock
-        # - Release the lock
-        # - Repeat until some condition is met (e.g. timeout, wakeupcounter == 3)
 
         self.wakeupcounter = 0
         while self.wakeupcounter <= 2: # Termination criteria
@@ -81,7 +105,7 @@ class Node(Thread):
             self.server.grants_received.clear()
 
             # Increase timestamp
-            # self.lamport_ts += 1
+            self.lamport_ts += 1
 
             # Send requests to all quorum peers
             self.client.multicast(
